244c607c17cc94983bbf7e875133695adb460308
lexcmcqueen <42482572+lexcmcqueen@users.noreply.github.com>
2019-02-12 20:55:51 -0500

    Added files to DiceRoller. Created initial layout, labels, and buttons.

---
+++ DiceRoller/DiceRoller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DiceRoller
+//
+//  Created by McQueen, Lexi on 2/12/19.
+//  Copyright © 2019 McQueen, Lexi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceValue: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
